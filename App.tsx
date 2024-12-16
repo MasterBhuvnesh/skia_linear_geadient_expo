916104523bdc06afd815488e7bcadd0694896c8b
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-12-16 23:33:53 +0530

    Its app using sika

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Blur, Canvas, RadialGradient, Rect } from "@shopify/react-native-skia";
+import { StatusBar } from "expo-status-bar";
+import { Dimensions, StyleSheet, View } from "react-native";
+
+const { width: WindowWidth, height: WindowHeight } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Canvas style={{ flex: 1 }}>
+        <Rect
+          x={0}
+          y={0}
+          width={WindowWidth}
+          height={WindowHeight}
+          color="blue"
+        >
+          <RadialGradient
+            c={{ x: WindowWidth / 2, y: WindowHeight / 2 }}
+            r={WindowWidth / 2}
+            colors={["violet", "black"]}
+          />
+          <Blur blur={100} />
+        </Rect>
+      </Canvas>
     </View>
   );
 }
@@ -13,8 +31,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
   },
 });
---
+++ README.md
@@ -0,0 +1,1 @@
+We have used sika from shopify to add linear gradient in our app
